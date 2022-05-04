36d13e18a984d6b3331c81abca4454779c1362b7
JoseAnayaR <joseanayautj@gmail.com>
2022-05-04 00:01:28 -0500

    3.3 Agregando requerimiento 3 error tratando de crear nuevo usuario

--- test/views/userViews.test.js
+++ test/views/userViews.test.js
@@ -12,6 +12,12 @@ describe("Test for UserView", () => {
         const payload = {username: null, name: 12, id: "id"}
         const result = UserView.createUser(payload)
         expect(result.error).toMatch(/necesitan tener un valor válido/)
-      })
+    })
+
+    test("Return an error object when try to create a new user with a payload with missing properties", () => {
+        const payload = {username: "Username"}
+        const result = UserView.createUser(payload)
+        expect(result.error).toMatch(/necesitan tener un valor válido/)
+    })
 
 })
