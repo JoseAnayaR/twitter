fbf9ed153d240348049df90702a417e3043d34b8
JoseAnayaR <joseanayautj@gmail.com>
2022-05-03 23:41:48 -0500

    3.2 Agregando requerimiento 2 necesitan un valor valido

--- test/views/userViews.test.js
+++ test/views/userViews.test.js
@@ -8,4 +8,10 @@ describe("Test for UserView", () => {
         expect(result.error).toMatch(/payload no existe/)
     })
 
+    test("Return an error object when try to create a new user with a payload with invalid properties", () => {
+        const payload = {username: null, name: 12, id: "id"}
+        const result = UserView.createUser(payload)
+        expect(result.error).toMatch(/necesitan tener un valor válido/)
+      })
+
 })
